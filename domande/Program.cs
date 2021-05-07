11accc12961e2a69ecb56ef11360162fe01d3c29
fradi <fradi@LAPTOP-IFORA1M9>
2021-05-07 13:16:13 +0200

    Aggiungere i file di progetto.

---
+++ domande/Program.cs
@@ -0,0 +1,51 @@
+﻿Francesca Dicugno
+
+1.	Nella memoria principale (o Memoria centrale) vengono caricati il programma e i dati necessari per eseguirlo e può essere a:
+-Breve - medio termine: informazione mantenuta per il tempo dell'esecuzione
+-Volatile: informazione persa se la macchina viene spenta.
+Esempi di memoria principale sono la RAM e la Cache.
+Nella Memoria secondari invece vengono caricati programmi e dati in maniera permanente e può essere a:
+-Lungo termine: informazione rimane in maniera permanente
+-Non volatile: contenuto della memoria non viene perso con lo spegnimento del computer.
+Esempi di memoria secondaria sono Hard Disk, CD, DVD.
+La memoria virtuale è un blocco di spazio su disco rigido o SSD che viene utilizzata quando la RAM fisica sta per esaurirsi durante l’esecuzione dei programmi.
+
+2.	Un linguaggio tippizzato garantisce che il valore inserito nella variabile sia utilizzato in maniera coerente rispetto al tipo della variabile. I tipi di base in C# sono:
+-	Float: singolo byte
+-Double: doppio byte
+-Int: memorizza gli interi
+-	Char: permette di memorizzare un singolo carattere
+-	Decimal: utilizzato per i valori monetari
+-	String: memorizza parole o frasi
+-	Boolean: true o false
+
+3.Un sistema operativo è un insieme di Softwares che serve per accedere alle risorse del computer, le sue funzionalità sono:
+-Gestione del processore(Kernel)
+-Gestione dell 'interfaccia per l'utente(bash, Interfaccia grafica, interfaccia vocale)
+ - Gestione della memoria
+-Gestione I/O
+
+4.	Gli operatori logici sono AND (&&) e OR(||) che hanno le rispettive tabelle di verità:
+-AND: il risultato è sempre FALSO tranne nel caso in cui i due valori siano uguali;
+-OR: il risultato è sempre VERO tranne nel caso in cui i due valori siano uguali.
+
+5.	Il cast è l'operazione di conversione di un tipo ad un altro affine.
+In caso di incompatibilità nella conversione viene lanciata una eccezione (ossia un errore) di tipo InvalidCastException.
+
+6.	Insieme di istruzioni racchiuse in un unico blocco di codice che formano un’entità richiamabile in altri punti del codice. L’elaborazione data dalla routine può avere:
+-Parametri di input
+-Valore di ritorno (risultato).
+Le routine si suddividono in:
+-Funzioni(hanno valore di ritorno)
+- Procedure(non hanno valore di ritorno).
+
+
+7.Il compilatore ha il compito di controllare la sintassi del linguaggio di programmazione e traduce il codice sorgente in codice macchina. Ma un codice che "compila" non è detto che sia corretto poichè il compilatore controlla solo la sintassi, non la logica.
+
+8.	Un sistema di versionamento permetti di registrare nel tempo I cambiamenti ad uno o più file      (es GIT).
+
+In particolare, permette di:
+-Ripristinare i file ad una versione precedente
+-Ripristinare un intero progetto ad una versione precedente
+-Revisionare le modifiche fatte nel tempo
+
